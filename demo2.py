c0a169e220a08693072ae67eb7e38a938d2d109e
zss <1271850087@qq.com>
2020-02-24 15:44:40 +0800

    新建python爬虫文件

---
+++ demo2.py
@@ -0,0 +1,5 @@
+#enconding: utf-8
+
+from urllib import request
+
+request.urlretrieve('https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1582527748&di=c79198dca3b80cff956d9726382d1cad&src=http://pic1.win4000.com/wallpaper/2018-01-30/5a7018031beb6.jpg','hero.jpg')
